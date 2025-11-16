3c929497462e19667b4592909230e9fece25a047
guilhemappadoo-stack <guilhem.appadoo@gmail.com>
2025-11-16 23:53:36 +0100

    Update bateau.py

--- bateau.py
+++ bateau.py
@@ -1,19 +1,13 @@
-from grille import Grille  
-
 class Bateau:
     def __init__(self, ligne, colonne, longueur=1, vertical=False, marque="B"):
-
         self.ligne = ligne
         self.colonne = colonne
         self.longueur = longueur
-
         self.vertical = vertical
-
         self.marque = marque
 
     @property
     def positions(self):
-
         resultat = []
         for i in range(self.longueur):
             if self.vertical:
@@ -23,13 +17,13 @@ class Bateau:
         return resultat
 
     def coule(self, grille):
-
         for (l, c) in self.positions:
             index = grille._index(l, c)
             if grille.grille[index] != "x":
                 return False
-        return True 
-    
+        return True
+
+
 class PorteAvion(Bateau):
     def __init__(self, ligne, colonne, vertical=False):
         super().__init__(ligne, colonne, longueur=4, vertical=vertical, marque="P")
